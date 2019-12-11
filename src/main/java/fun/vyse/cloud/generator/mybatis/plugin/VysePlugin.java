54e1104210d050e149c25aebf533faa789f16ff5
homeant <homeanter@163.com>
2019-12-11 23:30:09 +0800

    添加dto类生成

--- src/main/java/fun/vyse/cloud/generator/mybatis/plugin/VysePlugin.java
+++ src/main/java/fun/vyse/cloud/generator/mybatis/plugin/VysePlugin.java
@@ -4,18 +4,24 @@ import com.google.common.collect.Lists;
 import fun.vyse.cloud.generator.mybatis.plugin.comment.VyseCommentGenerator;
 import org.apache.commons.collections4.CollectionUtils;
 import org.apache.commons.lang3.StringUtils;
-import org.mybatis.generator.api.*;
-import org.mybatis.generator.api.dom.java.*;
+import org.mybatis.generator.api.GeneratedJavaFile;
+import org.mybatis.generator.api.IntrospectedColumn;
+import org.mybatis.generator.api.IntrospectedTable;
+import org.mybatis.generator.api.PluginAdapter;
+import org.mybatis.generator.api.dom.java.Field;
+import org.mybatis.generator.api.dom.java.FullyQualifiedJavaType;
+import org.mybatis.generator.api.dom.java.Interface;
+import org.mybatis.generator.api.dom.java.JavaVisibility;
+import org.mybatis.generator.api.dom.java.Method;
+import org.mybatis.generator.api.dom.java.TopLevelClass;
 import org.mybatis.generator.config.CommentGeneratorConfiguration;
 import org.mybatis.generator.config.Context;
 import org.mybatis.generator.config.DomainObjectRenamingRule;
-import org.mybatis.generator.config.ModelType;
 import org.mybatis.generator.internal.util.JavaBeansUtil;
 
-import java.io.Serializable;
+import java.util.Collections;
 import java.util.List;
 import java.util.Properties;
-import java.util.Set;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -39,6 +45,8 @@ public class VysePlugin extends PluginAdapter {
     private boolean comment = false;
 
     private boolean service = false;
+
+    private boolean dto = false;
 
     private String targetProject;
 
@@ -75,15 +83,15 @@ public class VysePlugin extends PluginAdapter {
     public boolean clientUpdateByPrimaryKeySelectiveMethodGenerated(Method method, Interface interfaze, IntrospectedTable introspectedTable) {
         StringBuffer buffer = new StringBuffer("updateBy");
         String methodName = getByPrimaryKeyMethodName(buffer, introspectedTable);
-        methodName = methodName+"Selective";
+        methodName = methodName + "Selective";
         method.setName(methodName);
         List<String> annotations = method.getAnnotations();
-        if(CollectionUtils.isNotEmpty(annotations)){
+        if (CollectionUtils.isNotEmpty(annotations)) {
             for (int i = 0; i < annotations.size(); i++) {
                 String annotation = annotations.get(i);
-                if(annotation.indexOf("@UpdateProvider")==0){
-                    annotation = annotation.replaceAll("updateByPrimaryKeySelective",methodName);
-                    annotations.set(i,annotation);
+                if (annotation.indexOf("@UpdateProvider") == 0) {
+                    annotation = annotation.replaceAll("updateByPrimaryKeySelective", methodName);
+                    annotations.set(i, annotation);
                 }
             }
         }
@@ -102,7 +110,7 @@ public class VysePlugin extends PluginAdapter {
     public boolean clientUpdateByPrimaryKeyWithBLOBsMethodGenerated(Method method, Interface interfaze, IntrospectedTable introspectedTable) {
         StringBuffer buffer = new StringBuffer("updateBy");
         String methodName = getByPrimaryKeyMethodName(buffer, introspectedTable);
-        methodName = methodName+"Blobs";
+        methodName = methodName + "Blobs";
         method.setName(methodName);
         return super.clientUpdateByPrimaryKeyWithBLOBsMethodGenerated(method, interfaze, introspectedTable);
     }
@@ -111,7 +119,7 @@ public class VysePlugin extends PluginAdapter {
     public boolean providerUpdateByPrimaryKeySelectiveMethodGenerated(Method method, TopLevelClass topLevelClass, IntrospectedTable introspectedTable) {
         StringBuffer buffer = new StringBuffer("updateBy");
         String methodName = getByPrimaryKeyMethodName(buffer, introspectedTable);
-        methodName = methodName+"Selective";
+        methodName = methodName + "Selective";
         method.setName(methodName);
         return super.providerUpdateByPrimaryKeySelectiveMethodGenerated(method, topLevelClass, introspectedTable);
     }
@@ -135,20 +143,12 @@ public class VysePlugin extends PluginAdapter {
     @Override
     public List<GeneratedJavaFile> contextGenerateAdditionalJavaFiles(IntrospectedTable introspectedTable) {
         List<GeneratedJavaFile> javaFiles = super.contextGenerateAdditionalJavaFiles(introspectedTable);
+        String domainObjectName = getDomainObjectName(introspectedTable);
         if (service) {
             if (CollectionUtils.isEmpty(javaFiles)) {
                 javaFiles = Lists.newArrayList();
             }
-            DomainObjectRenamingRule rule = introspectedTable.getTableConfiguration().getDomainObjectRenamingRule();
-            String domainObjectName = introspectedTable.getTableConfiguration().getTableName();
-            domainObjectName = JavaBeansUtil.getCamelCaseString(domainObjectName, true);
-            if (rule != null) {
-                Pattern pattern = Pattern.compile(rule.getSearchString());
-                String replaceString = rule.getReplaceString();
-                replaceString = replaceString == null ? "" : replaceString;
-                Matcher matcher = pattern.matcher(domainObjectName);
-                domainObjectName = matcher.replaceAll(replaceString);
-            }
+
             Interface serviceClass = new Interface(baseJavaPackage + ".service." + domainObjectName + "Service");
             serviceClass.setVisibility(JavaVisibility.PUBLIC);
             GeneratedJavaFile serviceFile = new GeneratedJavaFile(serviceClass, targetProject, context.getJavaFormatter());
@@ -160,40 +160,47 @@ public class VysePlugin extends PluginAdapter {
             serviceImplClass.addSuperInterface(serviceClass.getType());
             GeneratedJavaFile serviceImplFile = new GeneratedJavaFile(serviceImplClass, targetProject, context.getJavaFormatter());
             javaFiles.add(serviceImplFile);
-
-            TopLevelClass dtoReqClass = new TopLevelClass(baseJavaPackage + ".dto." + domainObjectName + "ReqDTO");
-            addAnnotation(dtoReqClass, introspectedTable);
-            dtoReqClass.addImportedType("java.io.Serializable");
-            dtoReqClass.addSuperInterface(new FullyQualifiedJavaType("java.io.Serializable"));
-            dtoReqClass.setVisibility(JavaVisibility.PUBLIC);
-            List<IntrospectedColumn> allColumns = introspectedTable.getAllColumns();
-            if (CollectionUtils.isNotEmpty(allColumns)) {
-                allColumns.stream().forEach(r -> {
-                    Field field = new Field(JavaBeansUtil.getJavaBeansField(r, this.context, introspectedTable));
-                    addFieldAnnotation(field, r);
-                    dtoReqClass.addField(field);
-                });
-            }
-            GeneratedJavaFile dtoReqFile = new GeneratedJavaFile(dtoReqClass, targetProject, context.getJavaFormatter());
-            javaFiles.add(dtoReqFile);
-
-            TopLevelClass dtoResultClass = new TopLevelClass(baseJavaPackage + ".dto." + domainObjectName + "ResultDTO");
-            addAnnotation(dtoResultClass, introspectedTable);
-            dtoResultClass.addImportedType("java.io.Serializable");
-            dtoResultClass.addSuperInterface(new FullyQualifiedJavaType("java.io.Serializable"));
-            dtoResultClass.setVisibility(JavaVisibility.PUBLIC);
-            if (CollectionUtils.isNotEmpty(allColumns)) {
-                allColumns.stream().forEach(r -> {
-                    Field field = new Field(JavaBeansUtil.getJavaBeansField(r, this.context, introspectedTable));
-                    addFieldAnnotation(field, r);
-                    dtoResultClass.addField(field);
-                });
-            }
-            GeneratedJavaFile dtoResultFile = new GeneratedJavaFile(dtoResultClass, targetProject, context.getJavaFormatter());
-            javaFiles.add(dtoResultFile);
-
         }
+        javaFiles.addAll(generatedDto(introspectedTable));
         return javaFiles;
+    }
+
+    private List<GeneratedJavaFile> generatedDto(IntrospectedTable introspectedTable) {
+        if (!dto) {
+            return Collections.emptyList();
+        }
+        String domainObjectName = getDomainObjectName(introspectedTable);
+        TopLevelClass dtoReqClass = new TopLevelClass(baseJavaPackage + ".dto." + domainObjectName + "ReqDTO");
+        addAnnotation(dtoReqClass, introspectedTable);
+        dtoReqClass.addImportedType("java.io.Serializable");
+        dtoReqClass.addSuperInterface(new FullyQualifiedJavaType("java.io.Serializable"));
+        dtoReqClass.setVisibility(JavaVisibility.PUBLIC);
+        List<IntrospectedColumn> allColumns = introspectedTable.getAllColumns();
+        if (CollectionUtils.isNotEmpty(allColumns)) {
+            allColumns.stream().forEach(r -> {
+                dtoReqClass.addImportedType(r.getFullyQualifiedJavaType());
+                Field field = new Field(JavaBeansUtil.getJavaBeansField(r, this.context, introspectedTable));
+                addFieldAnnotation(field, r);
+                dtoReqClass.addField(field);
+            });
+        }
+        GeneratedJavaFile dtoReqFile = new GeneratedJavaFile(dtoReqClass, targetProject, context.getJavaFormatter());
+
+        TopLevelClass dtoResultClass = new TopLevelClass(baseJavaPackage + ".dto." + domainObjectName + "ResultDTO");
+        addAnnotation(dtoResultClass, introspectedTable);
+        dtoResultClass.addImportedType("java.io.Serializable");
+        dtoResultClass.addSuperInterface(new FullyQualifiedJavaType("java.io.Serializable"));
+        dtoResultClass.setVisibility(JavaVisibility.PUBLIC);
+        if (CollectionUtils.isNotEmpty(allColumns)) {
+            allColumns.stream().forEach(r -> {
+                dtoResultClass.addImportedType(r.getFullyQualifiedJavaType());
+                Field field = new Field(JavaBeansUtil.getJavaBeansField(r, this.context, introspectedTable));
+                addFieldAnnotation(field, r);
+                dtoResultClass.addField(field);
+            });
+        }
+        GeneratedJavaFile dtoResultFile = new GeneratedJavaFile(dtoResultClass, targetProject, context.getJavaFormatter());
+        return Lists.newArrayList(dtoReqFile, dtoResultFile);
     }
 
     @Override
@@ -216,7 +223,7 @@ public class VysePlugin extends PluginAdapter {
 
     }
 
-    private String getByPrimaryKeyMethodName(StringBuffer buffer,IntrospectedTable introspectedTable){
+    private String getByPrimaryKeyMethodName(StringBuffer buffer, IntrospectedTable introspectedTable) {
         List<IntrospectedColumn> columns = introspectedTable.getPrimaryKeyColumns();
         for (int i = 0; i < columns.size(); i++) {
             IntrospectedColumn column = columns.get(i);
@@ -237,8 +244,23 @@ public class VysePlugin extends PluginAdapter {
         lombokBuilder = getPropertyAsBoolean(properties, "lombokBuilder", false);
         swagger = getPropertyAsBoolean(properties, "swagger", false);
         service = getPropertyAsBoolean(properties, "service", false);
+        dto = getPropertyAsBoolean(properties, "dto", false);
         super.context.getCommentGeneratorConfiguration().addProperty("lombok", getPropertyAsString(properties, "lombok"));
         super.context.getCommentGeneratorConfiguration().addProperty("swagger", getPropertyAsString(properties, "swagger"));
+    }
+
+    private String getDomainObjectName(IntrospectedTable introspectedTable) {
+        DomainObjectRenamingRule rule = introspectedTable.getTableConfiguration().getDomainObjectRenamingRule();
+        String domainObjectName = introspectedTable.getTableConfiguration().getTableName();
+        domainObjectName = JavaBeansUtil.getCamelCaseString(domainObjectName, true);
+        if (rule != null) {
+            Pattern pattern = Pattern.compile(rule.getSearchString());
+            String replaceString = rule.getReplaceString();
+            replaceString = replaceString == null ? "" : replaceString;
+            Matcher matcher = pattern.matcher(domainObjectName);
+            domainObjectName = matcher.replaceAll(replaceString);
+        }
+        return domainObjectName;
     }
 
     private void addAnnotation(TopLevelClass topLevelClass, IntrospectedTable introspectedTable) {
